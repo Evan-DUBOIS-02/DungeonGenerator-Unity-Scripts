24c2fad1582d9ec59890d8b2b92b92fd5c514baa
Evan DUBOIS <72149631+Evan-DUBOIS-02@users.noreply.github.com>
2025-07-29 08:48:54 +0200

    Add files via upload

---
+++ Graph.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace ProjectRuneUtils
+{
+    public class Edge
+    {
+        public Tile _node1;
+        public Tile _node2;
+        public float _weight;
+        public bool _isSelected; // used for MST
+    }
+    
+    public class Graph
+    {
+        public List<Tile> _nodes;
+        public List<Edge> _edges;
+    }
+
+    public static class GraphManager
+    {
+        public static Graph ComputeDelaunayTriangulationFromVoronoiGrid(Tile[,] grid, int gridSize, Tile[] germsPosition)
+        {
+            Graph graph = new Graph();
+            graph._edges = new List<Edge>();
+            graph._nodes = germsPosition.ToList();
+            
+            List<Tuple<int, int>> _existingEdges = new List<Tuple<int, int>>();
+            
+            for(int i = 1; i < gridSize; i++)
+            for (int j = 1; j < gridSize; j++)
+            {
+                int currentVornoiGrp = grid[i, j]._voronoiGrp;
+                
+                int otherVornoiGrp = grid[i - 1, j]._voronoiGrp;
+                if (otherVornoiGrp != currentVornoiGrp && !_existingEdges.Contains(new Tuple<int, int>(otherVornoiGrp, currentVornoiGrp)) &&  !_existingEdges.Contains(new Tuple<int, int>(currentVornoiGrp, otherVornoiGrp)))
+                {
+                    Edge edge = new Edge();
+                    edge._node1 =  germsPosition[currentVornoiGrp];
+                    edge._node2 = germsPosition[otherVornoiGrp];
+                    edge._weight = Vector2.Distance(germsPosition[currentVornoiGrp]._position,  germsPosition[otherVornoiGrp]._position);
+                        
+                    graph._edges.Add(edge);
+                    _existingEdges.Add(new Tuple<int, int>(otherVornoiGrp, currentVornoiGrp));
+                }
+                
+                otherVornoiGrp = grid[i, j-1]._voronoiGrp;
+                if (otherVornoiGrp != currentVornoiGrp && !_existingEdges.Contains(new Tuple<int, int>(otherVornoiGrp, currentVornoiGrp)) &&  !_existingEdges.Contains(new Tuple<int, int>(currentVornoiGrp, otherVornoiGrp)))
+                {
+                    Edge edge = new Edge();
+                    edge._node1 =  germsPosition[currentVornoiGrp];
+                    edge._node2 = germsPosition[otherVornoiGrp];
+                    edge._weight = Vector2.Distance(germsPosition[currentVornoiGrp]._position,  germsPosition[otherVornoiGrp]._position);
+                        
+                    graph._edges.Add(edge);
+                    _existingEdges.Add(new Tuple<int, int>(otherVornoiGrp, currentVornoiGrp));
+                }
+            }
+            
+            return graph;
+        }
+
+        public static Graph FindMST(Graph graph)
+        {
+            Edge random_edge = graph._edges[Random.Range(0, graph._edges.Count-1)];
+            Tile startingNode;
+            if(Random.Range(0, 100) < 50)
+                startingNode = random_edge._node1;
+            else
+                startingNode = random_edge._node2;
+            
+            List<Tile> _selectedNodes = new List<Tile>();
+            _selectedNodes.Add(startingNode);
+            
+            while (_selectedNodes.Count < graph._nodes.Count)
+            {
+                float minWeight = float.MaxValue;
+                int selectedEdgeIdx = -1;
+                
+                for(int edgeIdx = 0; edgeIdx < graph._edges.Count; edgeIdx++)
+                {
+                    if (!graph._edges[edgeIdx]._isSelected)
+                    {
+                        if ((_selectedNodes.Contains(graph._edges[edgeIdx]._node1) && !_selectedNodes.Contains(graph._edges[edgeIdx]._node2)) ||
+                            (!_selectedNodes.Contains(graph._edges[edgeIdx]._node1) && _selectedNodes.Contains(graph._edges[edgeIdx]._node2)))
+                        {
+                            if (graph._edges[edgeIdx]._weight < minWeight)
+                            {
+                                minWeight = graph._edges[edgeIdx]._weight;
+                                selectedEdgeIdx = edgeIdx;
+                            }
+                        }
+                    }
+                }
+                graph._edges.ToArray()[selectedEdgeIdx]._isSelected = true;
+                if(!_selectedNodes.Contains(graph._edges[selectedEdgeIdx]._node1))
+                    _selectedNodes.Add(graph._edges[selectedEdgeIdx]._node1);
+                else
+                    _selectedNodes.Add(graph._edges[selectedEdgeIdx]._node2);
+            }
+            
+            Graph mst =  new Graph();
+            mst._edges = new List<Edge>();
+            foreach (Edge edge in graph._edges)
+            {
+                if(edge._isSelected)
+                    mst._edges.Add(edge);
+            }
+
+            return mst;
+        }
+    }
+}
---
+++ PathFinding.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProjectRuneUtils
+{
+    public static class PathFinding
+    {
+        private static Dictionary<TileType, int> AStarCost = new Dictionary<TileType, int>
+        {
+            { TileType.Empty, 5},
+            { TileType.Room, 10},
+            { TileType.Hallway, 1}
+        };
+        
+        public static void ApplyAStarFromMSTGraph(Graph graph, Tile[,] grid, int gridSize)
+        {
+            foreach (Edge e in graph._edges)
+            {
+                List<Tile> path = PathFinding.FindPath(e._node1, e._node2, grid, gridSize);
+                foreach(Tile t in path)
+                {
+                    if(t._type == TileType.Empty)
+                        t._type = TileType.Hallway;
+                }
+            }
+        }
+        
+        private static List<Tile> FindPath(Tile start, Tile end, Tile[,] grid, int gridSize)
+        {
+            List<Tile> openList = new List<Tile>() {start};
+            HashSet<Tile> closedList = new HashSet<Tile>();
+            while (openList.Count > 0)
+            {
+                Tile currentTile =  openList[0];
+                for (int i = 1; i < openList.Count; i++)
+                {
+                    if (AStarCost[openList[i]._type] < AStarCost[currentTile._type] ||
+                        AStarCost[openList[i]._type] == AStarCost[currentTile._type] && openList[i].hCost < currentTile.hCost)
+                    {
+                        currentTile = openList[i];
+                    }
+                }
+                
+                openList.Remove(currentTile);
+                closedList.Add(currentTile);
+
+                if (currentTile == end)
+                {
+                    return RetracePath(start, end);
+                }
+
+                foreach (Tile neighbor in GetNeighbors(currentTile, grid, gridSize))
+                {
+                    if (closedList.Contains(neighbor)) continue;
+                    int newMovementCostToNeighbor = currentTile.gCost + GetDistance(currentTile, neighbor);
+                    if (newMovementCostToNeighbor < neighbor.gCost || !openList.Contains(neighbor))
+                    {
+                        neighbor.gCost = newMovementCostToNeighbor;
+                        neighbor.hCost = GetDistance(neighbor, end);
+                        neighbor.parent = currentTile;
+
+                        if (!openList.Contains(neighbor))
+                        {
+                            openList.Add(neighbor);
+                        }
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        private static int GetDistance(Tile nodeA, Tile nodeB)
+        {
+            int distanceX = Mathf.Abs((int)nodeA._position.x - (int)nodeB._position.x);
+            int distanceY = Mathf.Abs((int)nodeA._position.y - (int)nodeB._position.y);
+
+            if (distanceX > distanceY)
+            {
+                return 14 * distanceY + 10 * (distanceX - distanceY);
+            }
+            else
+            {
+                return 14 * distanceX + 10 * (distanceY - distanceX);
+            }
+        }
+
+        private static List<Tile> GetNeighbors(Tile node, Tile[,] grid,  int gridSize)
+        {
+            List<Tile> neighbors = new List<Tile>();
+            if(node._position.x - 1 >= 0)
+                neighbors.Add(grid[(int)node._position.x - 1, (int)node._position.y]);
+            if(node._position.x + 1 < gridSize)
+                neighbors.Add(grid[(int)node._position.x + 1, (int)node._position.y]);
+            if(node._position.y - 1 >= 0)
+                neighbors.Add(grid[(int)node._position.x, (int)node._position.y - 1]);
+            if(node._position.y + 1 < gridSize)
+                neighbors.Add(grid[(int)node._position.x, (int)node._position.y + 1]);
+
+            return neighbors;
+        }
+
+        private static List<Tile> RetracePath(Tile startNode, Tile endNode)
+        {
+            List<Tile> path = new List<Tile>();
+            Tile currentNode = endNode;
+
+            while (currentNode != startNode)
+            {
+                path.Add(currentNode);
+                currentNode = currentNode.parent;
+            }
+
+            path.Reverse();
+            return path;
+        }
+    }
+}

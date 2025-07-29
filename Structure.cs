e2ce253b6dde01cb84909c0e70b6d849b2db78aa
Evan DUBOIS <72149631+Evan-DUBOIS-02@users.noreply.github.com>
2025-07-29 08:50:17 +0200

    Add files via upload

---
+++ Structure.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+namespace DungeonGenerator
+{
+    public class Structure: MonoBehaviour
+    {
+        [SerializeField] private GameObject ZposDoor;
+        [SerializeField] private GameObject ZnegDoor;
+        [SerializeField] private GameObject XposDoor;
+        [SerializeField] private GameObject XnegDoor;
+
+        private Vector2Int _roomIndex;
+        public Vector2Int RoomIndex { get; set; }
+
+        public void OpenDoor(Vector2Int direction)
+        {
+            if(direction == Vector2Int.left)
+                XnegDoor.SetActive(false);
+            else if(direction == Vector2Int.right)
+                XposDoor.SetActive(false);
+            else if(direction == Vector2Int.down)
+                ZnegDoor.SetActive(false);
+            else if(direction == Vector2Int.up)
+                ZposDoor.SetActive(false);
+        }
+        
+        public void OpenZposDoor()
+        {
+            
+        }
+        
+        public void OpenZnegDoor()
+        {
+            
+        }
+        
+        public void OpenXposDoor()
+        {
+            
+        }
+        
+        public void OpenXnegDoor()
+        {
+            
+        }
+    }
+}
